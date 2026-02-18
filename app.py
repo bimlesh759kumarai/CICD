ad350587c84c3473e7c7870820cea446973f57c2
Bimlesh <bimlesh759kumar.ai@gmail.com>
2026-02-18 20:04:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+if __name__ == "__main__":
+    x = float(input("Enter first number: "))
+    y = float(input("Enter second number: "))
+    print("Result:", add(x, y))
